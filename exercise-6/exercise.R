3f8f4b191f56a2a366bc0bd92738ce870c83e513
David Lee <david168@uw.edu>
2017-01-26 15:19:28 -0800

    work on exercise

--- exercise-6/exercise.R
+++ exercise-6/exercise.R
@@ -3,13 +3,16 @@
 # Install the nycflights13 package and read it in. Require the dplyr package.
 # install.packages("nycflights13")
 
-
-
-# Create a dataframe of the average arrival delay for each destination, then use left_join
+# Create a dataframe of the average arrival delay for each destination, then use `left_join()`
 # to join on the "airports" dataframe, which has the airport info
+avg.arrival.delay <- flights %>%
+  group_by(dest) %>%
+  summarise(avg.delay = mean(arr_delay, na.rm = TRUE)) %>%
+  mutate(faa = dest) %>%
+  left_join(airports, by = 'faa') %>%
+  arrange(-avg.delay)
 
-
-# Create a dataframe of the average arrival delay for each airline, then use left_join
+# Create a dataframe of the average arrival delay for each airline, then use `left_join()``
 # to join on the "airlines" dataframe, which has the airline info
 
 
